76d2653fc9165b6b537f1f0ed113253c03156265
math-campos <matheuscampos2021@gmail.com>
2022-11-13 11:31:14 -0300

    Revert "seguranca"
    This reverts commit 43120bcfa800509f1fd394c57794509eb5be97c1.

--- banco.sql
+++ banco.sql
@@ -13,18 +13,3 @@ create table jogo (
 	produtora integer not null, 
 	foreign key (produtora) references produtora (codigo)
 );
-
--- criação da tabela usuários
-create table usuarios (
-	email varchar(50) not null primary key, 
-	senha varchar(20) not null, 
-	tipo char(1)  not null, 
-	check (tipo = 'T' or tipo = 'A' or tipo = 'U'),
-	telefone varchar(14)  not null, 
-	nome varchar(50) not null
-);
-
--- inserindo alguns registros na tabela usuários
-insert into usuarios (email, senha, tipo, telefone, nome) 
-values ('matheus@teste.com', '123456', 'A','(54)99999-9999','Matheus Campos'), 
-('augusto@teste.com', '123456', 'U','(54)66666-6666','Augusto Campos');
--- rotas.js
+++ rotas.js
@@ -2,30 +2,26 @@ const { Router } = require('express');
 
 const controleProdutoras = require('./controladores/produtoras');
 const controleJogos = require("./controladores/jogos");
-const seguranca = require('./controladores/seguranca');
 
 const rotas = new Router();
 
-rotas.route("/login")
-     .post(seguranca.login)
-
 rotas.route('/jogos')
-   .get(seguranca.verificaJWT, controleJogos.getJogos)
-   .post(seguranca.verificaJWT, controleJogos.addJogo)
-   .put(seguranca.verificaJWT, controleJogos.updateJogo)
+   .get(controleJogos.getJogos)
+   .post(controleJogos.addJogo)
+   .put(controleJogos.updateJogo)
 
 rotas.route('/jogos/:codigo')
-   .get(seguranca.verificaJWT, controleJogos.getJogoPorCodigo)
-   .delete(seguranca.verificaJWT, controleJogos.deleteJogo)
+   .get(controleJogos.getJogoPorCodigo)
+   .delete(controleJogos.deleteJogo)
 
 
 rotas.route('/produtoras')
-     .get(seguranca.verificaJWT, controleProdutoras.getProdutoras)
-     .post(seguranca.verificaJWT, controleProdutoras.addProdutora)
-     .put(seguranca.verificaJWT, controleProdutoras.updateProdutora)
+     .get(controleProdutoras.getProdutoras)
+     .post(controleProdutoras.addProdutora)
+     .put(controleProdutoras.updateProdutora)
 
 rotas.route('/produtoras/:codigo')
-     .get(seguranca.verificaJWT, controleProdutoras.getProdutoraPorCodigo)
-     .delete(seguranca.verificaJWT, controleProdutoras.deleteProdutora)
+     .get(controleProdutoras.getProdutoraPorCodigo)
+     .delete(controleProdutoras.deleteProdutora)
 
 module.exports = rotas;
